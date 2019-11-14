4023e139537653e8b87ef7cd41011537059464d3
Kevin DeLand <kevin.deland@gmail.com>
2019-11-14 16:21:18 -0500

    3.2.10 - InterventionShield covers game with translucent shield when intervention happens

---
+++ app/src/main/java/cmu/xprize/robotutor/tutorengine/widgets/core/TInterventionShield.java
@@ -0,0 +1,83 @@
+package cmu.xprize.robotutor.tutorengine.widgets.core;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import cmu.xprize.comp_intervention.views.CInterventionShield;
+import cmu.xprize.comp_logging.ILogManager;
+import cmu.xprize.robotutor.tutorengine.CTutor;
+import cmu.xprize.robotutor.tutorengine.ITutorGraph;
+import cmu.xprize.robotutor.tutorengine.ITutorObject;
+import cmu.xprize.robotutor.tutorengine.ITutorSceneImpl;
+
+/**
+ * RoboTutor
+ * <p>
+ * Created by kevindeland on 2019-11-14.
+ */
+public class TInterventionShield extends CInterventionShield implements ITutorObject {
+    public TInterventionShield(Context context) {
+        super(context);
+    }
+
+    public TInterventionShield(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TInterventionShield(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    /* This is bad, freakin' ITutorObject */
+    @Override
+    public void init(Context context, AttributeSet attrs) {
+
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void setVisibility(String visible) {
+
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public void setParent(ITutorSceneImpl mParent) {
+
+    }
+
+    @Override
+    public void setTutor(CTutor tutor) {
+
+    }
+
+    @Override
+    public void setNavigator(ITutorGraph navigator) {
+
+    }
+
+    @Override
+    public void setLogManager(ILogManager logManager) {
+
+    }
+}
---
+++ comp_intervention/src/main/java/cmu/xprize/comp_intervention/views/CInterventionShield.java
@@ -0,0 +1,118 @@
+package cmu.xprize.comp_intervention.views;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.AttributeSet;
+import android.view.View;
+
+import static cmu.xprize.util.TCONST.EXIT_FROM_INTERVENTION;
+import static cmu.xprize.util.TCONST.I_CANCEL_GESTURE;
+import static cmu.xprize.util.TCONST.I_CANCEL_HESITATE;
+import static cmu.xprize.util.TCONST.I_CANCEL_STUCK;
+import static cmu.xprize.util.TCONST.I_TRIGGER_FAILURE;
+import static cmu.xprize.util.TCONST.I_TRIGGER_GESTURE;
+import static cmu.xprize.util.TCONST.I_TRIGGER_HESITATE;
+import static cmu.xprize.util.TCONST.I_TRIGGER_STUCK;
+
+/**
+ * RoboTutor
+ * <p>
+ * Created by kevindeland on 2019-11-14.
+ */
+public class CInterventionShield extends View {
+
+    private Context  mContext;
+    private LocalBroadcastManager bManager;
+
+    private final static float OPACITY = 0.6f;
+
+    public CInterventionShield(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CInterventionShield(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CInterventionShield(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        this.mContext = context;
+
+        initView();
+        initReceiver();
+    }
+
+    /**
+     * Initialize View values
+     */
+    private void initView() {
+        this.setVisibility(INVISIBLE);
+        this.setBackgroundColor(Color.BLACK);
+        this.setAlpha(OPACITY);
+    }
+
+    /**
+     * Initialize Receiver values
+     */
+    private void initReceiver() {
+        bManager = LocalBroadcastManager.getInstance(mContext);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(I_TRIGGER_GESTURE);
+        filter.addAction(I_TRIGGER_HESITATE);
+        filter.addAction(I_TRIGGER_STUCK);
+        filter.addAction(I_TRIGGER_FAILURE);
+
+        filter.addAction(I_CANCEL_STUCK);
+        filter.addAction(I_CANCEL_HESITATE);
+        filter.addAction(I_CANCEL_GESTURE);
+
+        filter.addAction(EXIT_FROM_INTERVENTION);
+
+        bManager.registerReceiver(new InterventionShieldReceiver(), filter);
+    }
+
+    private void hideMe() {
+        this.setVisibility(View.INVISIBLE);
+    }
+
+    private void showMe() {
+        this.setVisibility(View.VISIBLE);
+    }
+
+    private class InterventionShieldReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            if (action == null) return;
+
+            switch(action) {
+                case I_TRIGGER_GESTURE:
+                case I_TRIGGER_HESITATE:
+                case I_TRIGGER_STUCK:
+                case I_TRIGGER_FAILURE:
+                    showMe();
+                    break;
+
+                case I_CANCEL_HESITATE:
+                case I_CANCEL_STUCK:
+                case I_CANCEL_GESTURE:
+                case EXIT_FROM_INTERVENTION:
+                    hideMe();
+                    break;
+            }
+        }
+    }
+}
