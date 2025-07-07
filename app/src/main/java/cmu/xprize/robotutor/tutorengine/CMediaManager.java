3c5b48e5fec3b9adcaeacfca047cd4c5f27f86e7
JackMostow <JackMostow@users.noreply.github.com>
2025-07-07 10:58:53 -0500

    Update to include development branch changes (#158)
    * "Please tap.mp3" (#155)
    
    Co-authored-by: triyasghosh <triyasghosh922@gmail.com>
    
    * 2025 06 10 triyas fix english version (#157)
    
    * "Please tap.mp3"
    
    * merge development branch into "Please tap.mp3" (#155) (#156)
    
    Co-authored-by: JackMostow <JackMostow@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: JackMostow <JackMostow@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: triyasghosh <triyasghosh922@gmail.com>
    Co-authored-by: ghosh-triyas <122919909+ghosh-triyas@users.noreply.github.com>

--- app/src/main/java/cmu/xprize/robotutor/tutorengine/CMediaManager.java
+++ app/src/main/java/cmu/xprize/robotutor/tutorengine/CMediaManager.java
@@ -246,7 +246,7 @@ public class CMediaManager {
         catch(Exception e) {
             soundPackage = autoLang;
         }
-
+        Log.d(TAG, "Resolved soundPackage = " + soundPackage);
         return soundPackage;
     }
 
@@ -285,7 +285,7 @@ public class CMediaManager {
         catch(Exception e) {
             packagePath = TCONST.BASE_ASSETS;
         }
-
+        Log.d(TAG, "Resolved packagePath = " + packagePath);
         return packagePath;
     }
 
@@ -324,7 +324,7 @@ public class CMediaManager {
         catch(Exception e) {
             packageLocation = TCONST.EXTERNAL;
         }
-
+        Log.d(TAG, "Resolved packageLocation = " + packageLocation);
         return packageLocation;
     }
 
